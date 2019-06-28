4b457c732f7cc5a6ad994cfbb733d17591293611
Roy <roy200230034004@gmail.com>
2019-06-28 20:47:30 +0800

    test component

---
+++ Scripts/testjsx.jsx
@@ -0,0 +1,4 @@
+ReactDom.render(
+<h1>Hello Kitty!</h1>,
+getElementById('babel-test')
+);
--- Scripts/trial.js
+++ Scripts/trial.js
@@ -8,7 +8,7 @@ class LikeButton extends React.Component {
 
 	render(){
 		if(this.state.liked){
-			return 'you likes this';
+			return 'you likes this'
 		}
 
 		return e (
@@ -21,5 +21,36 @@ class LikeButton extends React.Component {
 	}
 }
 
+class Test extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			a: this.props.showText,
+		};
+	}
+	render(){
+		// return e(
+		// 		'h3',
+		// 		{},
+		// 		this.state.a
+		// 	);
+		return e(Child,{showText: this.state.a});
+	}
+}
+
+class Child extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return e(
+				'button',
+				{},
+				this.props.showText
+			)
+	}
+}
+
 const app = document.querySelector('#app');
-ReactDOM.render(e(LikeButton), app);
+ReactDOM.render(e(Test, {showText : 'grandPaText'} ), app);
+
